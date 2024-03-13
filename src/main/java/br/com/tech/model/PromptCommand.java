41c17b399aeb410e167b697d637bda4609ffc9d9
Augusto Calisto <augustoaquino007@gmail.com>
2024-03-12 21:59:59 -0300

    Criacao classe para executar o command correto

---
+++ src/main/java/br/com/tech/model/PromptCommand.java
@@ -0,0 +1,47 @@
+package br.com.tech.model;
+
+import java.io.IOException;
+
+public class PromptCommand {
+    private static final String SISTEMA_OPERACIONAL = System.getProperty("os.name");
+	
+    private ISistemaOperacional sistemaOperacional;
+	
+    public PromptCommand() {
+        if(SISTEMA_OPERACIONAL.contains("Windows")) {
+            sistemaOperacional = new Windows();
+        }
+
+        if(SISTEMA_OPERACIONAL.contains("Linux")) {
+           sistemaOperacional = new Linux();
+        }
+    }
+	
+    public long desligarComputador(long tempo) throws IOException {
+        if(tempo < 0) {
+            throw new IllegalArgumentException("A data/hora passada é menor que o horário atual do computador");
+        }
+
+        return sistemaOperacional.desligar(tempo);
+    }
+    
+    public long reiniciarComputador(long tempo) throws IOException {
+        if(tempo < 0) {
+            throw new IllegalArgumentException("A data/hora passada é menor que o horário atual do computador");
+        }
+
+        return sistemaOperacional.reiniciar(tempo);
+    }
+
+    public long cancelarDesligamento() throws IOException {
+        return sistemaOperacional.cancelar();
+    }
+
+    public long hibernarComputador(long tempo) throws IOException {
+        if(tempo < 0) {
+            throw new IllegalArgumentException("A data/hora passada é menor que o horário atual do computador");
+        }
+        
+        return sistemaOperacional.hibernar(tempo);
+    }
+}
